15b0c6e3e118763ef42976485ad095ba2f1ef0b8
anusheel <anusheel@gmail.com>
2023-11-04 10:20:45 +0000

    Commit from Stub AI

--- components/ContactUsForm.tsx
+++ components/ContactUsForm.tsx
@@ -29,25 +29,25 @@ const ContactUsForm: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center bg-white shadow-md rounded-lg p-6 hover:shadow-lg transition-shadow duration-300">
-      <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
+    <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center bg-gradient-to-r from-blue-500 to-green-500 shadow-md rounded-lg p-6 hover:shadow-lg transition-shadow duration-300">
+      <h2 className="text-2xl font-bold mb-4 text-white">Contact Us</h2>
       <div className="flex items-center mb-4">
-        <label htmlFor="name" className="mr-2 text-gray-700">Name</label>
+        <label htmlFor="name" className="mr-2 text-white">Name</label>
         <input type="text" id="name" value={name} onChange={handleNameChange} className="border-2 border-gray-200 rounded-md p-1 w-64" required />
       </div>
       <div className="flex items-center mb-4">
-        <label htmlFor="email" className="mr-2 text-gray-700">Email</label>
+        <label htmlFor="email" className="mr-2 text-white">Email</label>
         <input type="email" id="email" value={email} onChange={handleEmailChange} className="border-2 border-gray-200 rounded-md p-1 w-64" required />
       </div>
       <div className="flex items-center mb-4">
-        <label htmlFor="date" className="mr-2 text-gray-700">Meeting Date</label>
+        <label htmlFor="date" className="mr-2 text-white">Meeting Date</label>
         <input type="date" id="date" value={date} onChange={handleDateChange} className="border-2 border-gray-200 rounded-md p-1 w-64" required />
       </div>
       <div className="flex items-start mb-4">
-        <label htmlFor="message" className="mr-2 text-gray-700">Message</label>
+        <label htmlFor="message" className="mr-2 text-white">Message</label>
         <textarea id="message" value={message} onChange={handleMessageChange} className="border-2 border-gray-200 rounded-md p-1 w-64" rows={4} />
       </div>
-      <button type="submit" className="bg-blue-500 text-white rounded-lg px-6 py-2 hover:bg-blue-600 transition-colors duration-300">Submit</button>
+      <button type="submit" className="bg-white text-blue-500 rounded-lg px-6 py-2 hover:bg-blue-600 hover:text-white transition-colors duration-300">Submit</button>
     </form>
   );
 };
